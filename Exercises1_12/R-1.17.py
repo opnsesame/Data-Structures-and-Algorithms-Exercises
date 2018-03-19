c7ae655457b0060a3a49c690a061a43b872059c5
opnsesame <passingcraze@hotmail.com>
2018-03-18 22:49:53 -0400

    variable scope
    #Had we implemented the scale function(page 25)as follows,does it work
    #properly?
    #def scale(data,factor):
    #   for val in data:
    #       val *= factor

---
+++ Exercises1_12/R-1.17.py
@@ -0,0 +1,9 @@
+#Had we implemented the scale function(page 25)as follows,does it work
+#properly?
+#def scale(data,factor):
+#   for val in data:
+#       val *= factor
+
+#the function won't change data. val is a local variable,it is not
+#an entry of data.
+
