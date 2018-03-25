7a9b3ba94759e9abcbc60f4dc8258198bdbc28e2
opnsesame <passingcraze@hotmail.com>
2018-03-25 12:49:32 -0400

    software adaptability
    Give an example of a software application in which adaptability can mean
    the difference between a prolonged lifetime of sales and bankruptcy

---
+++ Ecercises2/R-2.02.py
@@ -0,0 +1,6 @@
+'''
+Give an example of a software application in which adaptability can mean
+the difference between a prolonged lifetime of sales and bankruptcy
+
+The point of sales system that wont be able to change tax rate 
+'''
