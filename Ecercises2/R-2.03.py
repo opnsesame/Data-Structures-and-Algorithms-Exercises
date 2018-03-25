3920270b8242a2bffd11f9fe920664ad78e49384
opnsesame <passingcraze@hotmail.com>
2018-03-25 12:49:55 -0400

    encapsulate
    Describe a component from a text-editor GUI and the methods that it
    encapsulates.

---
+++ Ecercises2/R-2.03.py
@@ -0,0 +1,8 @@
+'''
+Describe a component from a text-editor GUI and the methods that it 
+encapsulates.
+
+1. open encapsulate read file method
+2. save encapsulate write file method
+3. search encapsulate string operation method
+'''
