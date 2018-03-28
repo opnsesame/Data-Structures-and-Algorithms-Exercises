e5864f8a4e02af1aeed912b1543b431a3088a04a
opnsesame <passingcraze@hotmail.com>
2018-03-27 21:46:51 -0400

    Vector __rmul__ method
    Exercise R-2.12 asks for an implementation of __mul__ , for the Vector
    class of Section 2.3.3, to provide support for the syntax v*3. Implement
    the rmul method, to provide additional support for syntax 3*v.

---
+++ Ecercises2/R-2.13.py
@@ -0,0 +1,11 @@
+'''
+Exercise R-2.12 asks for an implementation of mul , for the Vector
+class of Section 2.3.3, to provide support for the syntax v 3. Implement
+the rmul method, to provide additional support for syntax 3 v.
+'''
+def __mul__(self,n):
+	result = Vector(len(self))
+	for i in range(len(self)):
+		result[i] = self.[i]*n
+	return result
+__rmul__ = __mul__
