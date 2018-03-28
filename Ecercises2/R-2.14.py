7871308ec08a24f12e1e76faa90321bec22bdf46
opnsesame <passingcraze@hotmail.com>
2018-03-27 21:47:16 -0400

    Vector dot product method
    Implement the __mul__ method for the Vector class of Section 2.3.3, so
    that the expression u v returns a scalar that represents the dot product of
    the vectors, that is, Σ = ui · vi.

---
+++ Ecercises2/R-2.14.py
@@ -0,0 +1,13 @@
+'''
+Implement the __mul__ method for the Vector class of Section 2.3.3, so
+that the expression u v returns a scalar that represents the dot product of
+the vectors, that is, Σ = ui · vi.
+'''
+
+def __mul__(self,other):
+	if len(self) != len(other):
+		raise ValueError('dimensions must agree')
+	result = Vector(len(self))
+	for i in range(len(self)):
+		result[i] = self[i]*other[i]
+	return result
