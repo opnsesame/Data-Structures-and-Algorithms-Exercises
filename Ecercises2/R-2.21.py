59af47002dfcbc3a71ae5d62a539f3c59e3fe13c
opnsesame <passingcraze@hotmail.com>
2018-04-07 17:44:11 -0400

    R-2.21.py
    What are some potential efficiency disadvantages of having very shallow
    inheritance trees, that is, a large set of classes, A, B, C, and so on, such
    that all of these classes extend a single class, Z?

---
+++ Ecercises2/R-2.21.py
@@ -0,0 +1,8 @@
+'''
+What are some potential efficiency disadvantages of having very shallow
+inheritance trees, that is, a large set of classes, A, B, C, and so on, such
+that all of these classes extend a single class, Z?
+'''
+'''
+does not take advantage of the benefits on an inheritance hierarchy. So a bunch of classes extending one class becomes disorganized.
+'''
