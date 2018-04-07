309d251f52d3aa22696cbabd79c1ce82092b585c
opnsesame <passingcraze@hotmail.com>
2018-04-07 17:42:24 -0400

    Fibonacci progression
    Give a short fragment of Python code that uses the progression classes
    from Section 2.4.2 to find the 8th value of a Fibonacci progression that
    starts with 2 and 2 as its first two values.

---
+++ Ecercises2/R-2.18.py
@@ -0,0 +1,7 @@
+'''
+Give a short fragment of Python code that uses the progression classes
+from Section 2.4.2 to find the 8th value of a Fibonacci progression that
+starts with 2 and 2 as its first two values.
+'''
+fb = FibonacciProgression(2,2)
+fb.print_progresson(8,1)
