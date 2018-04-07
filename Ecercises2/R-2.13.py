29ea08589000a2caf78e3347931e2388d1c46b35
opnsesame <passingcraze@hotmail.com>
2018-04-07 17:37:21 -0400

    class method override
    '''
    Exercise R-2.12 asks for an implementation of __mul__ , for the Vector
    class of Section 2.3.3, to provide support for the syntax v*3. Implement
    the rmul method, to provide additional support for syntax 3*v.
    '''

--- Ecercises2/R-2.13.py
+++ Ecercises2/R-2.13.py
@@ -1,7 +1,7 @@
 '''
-Exercise R-2.12 asks for an implementation of mul , for the Vector
-class of Section 2.3.3, to provide support for the syntax v 3. Implement
-the rmul method, to provide additional support for syntax 3 v.
+Exercise R-2.12 asks for an implementation of __mul__ , for the Vector
+class of Section 2.3.3, to provide support for the syntax v*3. Implement
+the rmul method, to provide additional support for syntax 3*v.
 '''
 def __mul__(self,n):
 	result = Vector(len(self))
