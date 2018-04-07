72eaa4f94f20a0a3c825c767b37089439d6994df
opnsesame <passingcraze@hotmail.com>
2018-04-07 17:43:05 -0400

    R-2.19.py
    When using the ArithmeticProgression class of Section 2.4.2 with an increment
    of 128 and a start of 0, how many calls to next can we make
    before we reach an integer of 263 or larger?

---
+++ Ecercises2/R-2.19.py
@@ -0,0 +1,7 @@
+'''
+When using the ArithmeticProgression class of Section 2.4.2 with an increment
+of 128 and a start of 0, how many calls to next can we make
+before we reach an integer of 263 or larger?
+'''
+
+n=2^56
