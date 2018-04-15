49890c79a2e607a7815c6d2cb02fc2dbd3d90a6a
opnsesame <passingcraze@hotmail.com>
2018-04-15 13:12:13 -0400

    Show that n is O(nlogn).
    Show that n is O(nlogn).

---
+++ Exercises3/R-3.19.py
@@ -0,0 +1,9 @@
+'''
+Show that n is O(nlogn).
+'''
+n is o(n)
+
+         n                           1
+------------------------ = ---------------------  ->0  (n -> infinit)
+     n*log(n)                     log(n)
+hence n is O(nlogn)
