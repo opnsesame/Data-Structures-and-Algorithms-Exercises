b8272927a70d7c2f2b34aa3a802fa4967e8d25a7
opnsesame <passingcraze@hotmail.com>
2018-04-15 13:09:00 -0400

    Order the following functions by asymptotic growth rate
    Order the following functions by asymptotic growth rate.
    4nlog n+2n 210 2log n
    3n+100log n 4n 2n
    n2 +10n n3 nlog n

---
+++ Exercises3/R-3.08.py
@@ -0,0 +1,16 @@
+'''
+Order the following functions by asymptotic growth rate.
+4nlog n+2n 210 2log n
+3n+100log n 4n 2n
+n2 +10n n3 nlog n
+'''
+'''
+2^10                     O(1)
+3n+100log(n)             O(log(n))
+4n                       O(n)
+4nlog(n)+2n and          O(nlog(n))
+n^2+10n and              O(n^2)
+n^3                      O(n^3)
+2^(log(n))               O(2^(log(n)))
+2^n                      O(2^n)
+'''
